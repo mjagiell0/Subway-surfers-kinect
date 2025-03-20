49a84b25e0e50ad39df26221a5fba0693b02a9ec
jaguel <m.jagiello03@gmail.com>
2025-03-20 23:16:11 +0100

    readme and made video a bit faster

---
+++ README.md
@@ -0,0 +1,13 @@
+# Subway surfers controlled by moves from camera
+
+The project allows you to play Subway surfers using body movements. The image is divided into 5 areas, which correspond to the actions in the game:
+- **Center** - the character runs in the center
+- **Left** - the character runs on the left side
+- **Right** - the character runs on the right side
+- **Crouch** - the character does a somersault
+- **Hands up** - the character jumps
+
+**The gameplay is based on the browser version of the game at the link 
+[Subway surfers](https://subwaysurfersgithub.github.io/#)**
+
+The program that reads the new position of the character on the captured image sends a signal that maps the arrow signal
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 import cv2
 import mediapipe as mp
 
@@ -19,52 +21,58 @@ right_part = (width // 3) * 2
 position = "UNKNOWN"
 previous_position = "UNKNOWN"
 
+frame_counter = 0
+access_count = 10 # Ze względu na brak GPU. W przeciwnym wypadku wstawić 1
 while cap.isOpened():
     ret, frame = cap.read()
     if not ret:
         break
 
-    # Konwersja do RGB (MediaPipe wymaga takiego formatu)
-    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    if frame_counter % access_count == 0:
+        # Konwersja do RGB (MediaPipe wymaga takiego formatu)
+        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-    # Przetwarzanie klatki przez model
-    results = pose.process(rgb_frame)
+        # Przetwarzanie klatki przez model
+        results = pose.process(rgb_frame)
+
+
+
+        if results.pose_landmarks:
+            # Pobranie pozycji środka bioder
+            left_hip = results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_HIP]
+            right_hip = results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_HIP]
+
+            # Sprawdzenie, czy wykryto poprawne punkty
+            if left_hip.visibility > 0.5 and right_hip.visibility > 0.5:
+                # Obliczenie średniej pozycji X
+                center_x = int((left_hip.x + right_hip.x) / 2 * frame.shape[1])
+
+                # Określenie nowej pozycji
+                new_position = "UNKNOWN"
+                if center_x <= left_part:
+                    new_position = "LEFT"
+                elif center_x < right_part:
+                    new_position = "CENTRE"
+                else:
+                    new_position = "RIGHT"
+
+                # Zmieniamy `position` tylko, jeśli faktycznie zmieniła się strefa
+                if new_position != previous_position:
+                    # TODO: Dodać wysyłanie sygnałów strzałek do gry w przeglądarce
+                    position = new_position
+                    previous_position = new_position  # Aktualizacja historii pozycji
 
     # Rysowanie linii podziału
-    cv2.line(frame, (left_part, 0), (left_part, height), (255, 0, 0), 2)  # Lewa linia - niebieska
-    cv2.line(frame, (right_part, 0), (right_part, height), (255, 0, 0), 2)  # Prawa linia - niebieska
-
-    if results.pose_landmarks:
-        # Pobranie pozycji środka bioder
-        left_hip = results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_HIP]
-        right_hip = results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_HIP]
-
-        # Sprawdzenie, czy wykryto poprawne punkty
-        if left_hip.visibility > 0.5 and right_hip.visibility > 0.5:
-            # Obliczenie średniej pozycji X
-            center_x = int((left_hip.x + right_hip.x) / 2 * frame.shape[1])
-
-            # Określenie nowej pozycji
-            new_position = "UNKNOWN"
-            if center_x <= left_part:
-                new_position = "LEFT"
-            elif center_x < right_part:
-                new_position = "CENTRE"
-            else:
-                new_position = "RIGHT"
-
-            # Zmieniamy `position` tylko, jeśli faktycznie zmieniła się strefa
-            if new_position != previous_position:
-                position = new_position
-                previous_position = new_position  # Aktualizacja historii pozycji
-
+    cv2.line(frame, (left_part, 0), (left_part, height), (255, 0, 0), 2)
+    cv2.line(frame, (right_part, 0), (right_part, height), (255, 0, 0), 2)
     # Wyświetlenie pozycji na obrazie
     cv2.putText(frame, position, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
     cv2.imshow("Pose Tracking", frame)
-
     if cv2.waitKey(25) & 0xFF == ord("q"):
         break
+    frame_counter += 1
+
 
 cap.release()
 cv2.destroyAllWindows()
