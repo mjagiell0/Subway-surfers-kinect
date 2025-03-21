738d69a1e1a904ce386ee8a9188d3a4262486510
jaguel <m.jagiello03@gmail.com>
2025-03-21 13:54:37 +0100

    reading horizontal and vertical position

--- main.py
+++ main.py
@@ -9,17 +9,21 @@ pose = mp_pose.Pose()
 mp_drawing = mp.solutions.drawing_utils
 
 # Otwórz wideo
-cap = cv2.VideoCapture("assets/test2.mp4")
+cap = cv2.VideoCapture("assets/test3.mp4")
 
 width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
+upper_part = height // 5
+lower_part = height - upper_part
 left_part = width // 3
-right_part = (width // 3) * 2
+right_part = width - left_part
 
 # Inicjalizacja pozycji
-position = "UNKNOWN"
-previous_position = "UNKNOWN"
+pos_vertical = "UNKNOWN"
+pos_horizontal = "UNKNOWN"
+prev_pos_vertical = "UNKNOWN"
+prev_pos_horizontal = "UNKNOWN"
 
 frame_counter = 0
 access_count = 10 # Ze względu na brak GPU. W przeciwnym wypadku wstawić 1
@@ -33,40 +37,56 @@ while cap.isOpened():
         rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
         # Przetwarzanie klatki przez model
-        results = pose.process(rgb_frame)
+        body = pose.process(rgb_frame)
 
 
-
-        if results.pose_landmarks:
+        if body.pose_landmarks:
             # Pobranie pozycji środka bioder
-            left_hip = results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_HIP]
-            right_hip = results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_HIP]
+            left_hip = body.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_HIP]
+            right_hip = body.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_HIP]
+            left_thumb = body.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_THUMB]
+            right_thumb = body.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_THUMB]
 
             # Sprawdzenie, czy wykryto poprawne punkty
             if left_hip.visibility > 0.5 and right_hip.visibility > 0.5:
                 # Obliczenie średniej pozycji X
-                center_x = int((left_hip.x + right_hip.x) / 2 * frame.shape[1])
-
+                center_x_hip = int((left_hip.x + right_hip.x) / 2 * frame.shape[1])
+                center_y_thumbs = int((left_thumb.y + right_thumb.y) / 2 * frame.shape[0])
                 # Określenie nowej pozycji
-                new_position = "UNKNOWN"
-                if center_x <= left_part:
-                    new_position = "LEFT"
-                elif center_x < right_part:
-                    new_position = "CENTRE"
+                new_pos_vertical = "UNKNOWN"
+                if center_x_hip <= left_part:
+                    new_pos_vertical = "LEFT"
+                elif center_x_hip < right_part:
+                    new_pos_vertical = "CENTRE"
                 else:
-                    new_position = "RIGHT"
+                    new_pos_vertical = "RIGHT"
+
+                new_pos_horizontal = "UNKNOWN"
+                if center_y_thumbs <= upper_part:
+                    new_pos_horizontal = "JUMP"
+                elif center_y_thumbs < lower_part:
+                    new_pos_horizontal = "NOTHING"
+                else:
+                    new_pos_horizontal = "CROUCH"
 
                 # Zmieniamy `position` tylko, jeśli faktycznie zmieniła się strefa
-                if new_position != previous_position:
+                if new_pos_vertical != prev_pos_vertical:
                     # TODO: Dodać wysyłanie sygnałów strzałek do gry w przeglądarce
-                    position = new_position
-                    previous_position = new_position  # Aktualizacja historii pozycji
+                    pos_vertical = new_pos_vertical
+                    prev_pos_vertical = new_pos_vertical  # Aktualizacja historii pozycji
+
+                if new_pos_horizontal != prev_pos_horizontal:
+                    pos_horizontal = new_pos_horizontal
+                    prev_pos_horizontal = new_pos_horizontal
 
     # Rysowanie linii podziału
-    cv2.line(frame, (left_part, 0), (left_part, height), (255, 0, 0), 2)
-    cv2.line(frame, (right_part, 0), (right_part, height), (255, 0, 0), 2)
+    cv2.line(frame, (left_part, upper_part), (left_part, lower_part), (255, 0, 0), 2)
+    cv2.line(frame, (right_part, upper_part), (right_part, lower_part), (255, 0, 0), 2)
+    cv2.line(frame, (0, upper_part), (width, upper_part), (255, 0, 0), 2)
+    cv2.line(frame, (0, lower_part), (width, lower_part), (255, 0, 0), 2)
     # Wyświetlenie pozycji na obrazie
-    cv2.putText(frame, position, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+    cv2.putText(frame, pos_vertical, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+    cv2.putText(frame, pos_horizontal, (50, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
     cv2.imshow("Pose Tracking", frame)
     if cv2.waitKey(25) & 0xFF == ord("q"):
