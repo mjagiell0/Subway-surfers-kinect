7cd0b13cf2e4f4ffdf93ee6db5ec90bd0f7ea0b2
jaguel <m.jagiello03@gmail.com>
2025-03-21 22:10:48 +0100

    faster reaction in game

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-from time import sleep
-
 import cv2
 import mediapipe as mp
-import positions
 from positions import Horizontal, Vertical
 from pyautogui import press
+
 
 # Inicjalizacja modelu
 mp_pose = mp.solutions.pose
@@ -13,13 +11,16 @@ mp_drawing = mp.solutions.drawing_utils
 
 # Otwórz wideo
 cap = cv2.VideoCapture(0)
+# cv2.namedWindow("window", cv2.WND_PROP_FULLSCREEN)
+# cv2.setWindowProperty("window", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+
 
 width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-upper_part = height // 5
+upper_part = height // 3
 lower_part = height - upper_part
-left_part = width // 3
+left_part = width // 3 + 15
 right_part = width - left_part
 
 # Inicjalizacja pozycji
@@ -31,70 +32,73 @@ prev_pos_horizontal = Horizontal.NOTHING
 line_colour = (255, 0, 0)
 text_colour = (0, 255, 0)
 
-frame_counter = 0
-access_count = 10 # Ze względu na brak GPU. W przeciwnym wypadku wstawić 1
 while cap.isOpened():
     ret, frame = cap.read()
     if not ret:
         break
-    frame = cv2.flip(frame,1)
-    if frame_counter % access_count == 0:
-        # Konwersja do RGB (MediaPipe wymaga takiego formatu)
-        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    frame = cv2.flip(frame, 1)
 
-        # Przetwarzanie klatki przez model
-        body = pose.process(rgb_frame)
+    # Konwersja do RGB (MediaPipe wymaga takiego formatu)
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
+    # Przetwarzanie klatki przez model
+    body = pose.process(rgb_frame)
 
-        if body.pose_landmarks:
-            # Pobranie pozycji środka bioder
-            left_hip = body.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_HIP]
-            right_hip = body.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_HIP]
-            left_thumb = body.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_THUMB]
-            right_thumb = body.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_THUMB]
+    if body.pose_landmarks:
+        # Pobranie pozycji środka bioder
+        left_hip = body.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_HIP]
+        right_hip = body.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_HIP]
+        left_thumb = body.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_THUMB]
+        right_thumb = body.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_THUMB]
 
-            # Sprawdzenie, czy wykryto poprawne punkty
-            if left_hip.visibility > 0.5 and right_hip.visibility > 0.5:
-                # Obliczenie średniej pozycji X
-                center_x_hip = int((left_hip.x + right_hip.x) / 2 * frame.shape[1])
-                center_y_thumbs = int((left_thumb.y + right_thumb.y) / 2 * frame.shape[0])
-                # Określenie nowej pozycji
+        # Sprawdzenie, czy wykryto poprawne punkty
+        if left_hip.visibility > 0.5 and right_hip.visibility > 0.5:
+            # Obliczenie średniej pozycji X
+            center_x_hip = int((left_hip.x + right_hip.x) / 2 * frame.shape[1])
+            center_y_thumbs = int((left_thumb.y + right_thumb.y) / 2 * frame.shape[0])
+
+            cv2.circle(frame, (center_x_hip, int((left_hip.y + right_hip.y) / 2 * frame.shape[0])), 10, (0, 0, 255),
+                       -1)
+            cv2.circle(frame, (int((left_thumb.x + right_thumb.x) / 2 * frame.shape[1]), center_y_thumbs), 10,
+                       (0, 0, 255),
+                       -1)
+            # Określenie nowej pozycji
+            new_pos_vertical = Vertical.CENTER
+            if center_x_hip <= left_part:
+                new_pos_vertical = Vertical.LEFT
+            elif center_x_hip < right_part:
                 new_pos_vertical = Vertical.CENTER
-                if center_x_hip <= left_part:
-                    new_pos_vertical = Vertical.LEFT
-                elif center_x_hip < right_part:
-                    new_pos_vertical = Vertical.CENTER
-                else:
-                    new_pos_vertical = Vertical.RIGHT
+            else:
+                new_pos_vertical = Vertical.RIGHT
 
+            new_pos_horizontal = Horizontal.NOTHING
+            if center_y_thumbs <= upper_part:
+                new_pos_horizontal = Horizontal.UP
+            elif center_y_thumbs < lower_part:
                 new_pos_horizontal = Horizontal.NOTHING
-                if center_y_thumbs <= upper_part:
-                    new_pos_horizontal = Horizontal.UP
-                elif center_y_thumbs < lower_part:
-                    new_pos_horizontal = Horizontal.NOTHING
-                else:
-                    new_pos_horizontal = Horizontal.DOWN
+            else:
+                new_pos_horizontal = Horizontal.DOWN
 
-                # Zmieniamy `position` tylko, jeśli faktycznie zmieniła się strefa
-                if new_pos_vertical != prev_pos_vertical:
-                    if prev_pos_vertical == Vertical.RIGHT and new_pos_vertical == Vertical.CENTER:
-                        press("left")
-                    elif prev_pos_vertical == Vertical.LEFT and new_pos_vertical == Vertical.CENTER:
-                        press("right")
-                    elif prev_pos_vertical == Vertical.CENTER:
-                        press((str(new_pos_vertical)).lower())
+            # Zmieniamy `position` tylko, jeśli faktycznie zmieniła się strefa
+            if new_pos_vertical != prev_pos_vertical:
+                if prev_pos_vertical == Vertical.RIGHT and new_pos_vertical == Vertical.CENTER:
+                    press("left")
+                elif prev_pos_vertical == Vertical.LEFT and new_pos_vertical == Vertical.CENTER:
+                    press("right")
+                elif prev_pos_vertical == Vertical.CENTER:
+                    press((str(new_pos_vertical)).lower())
 
-                    pos_vertical = new_pos_vertical
-                    prev_pos_vertical = new_pos_vertical
+                pos_vertical = new_pos_vertical
+                prev_pos_vertical = new_pos_vertical
 
-                if new_pos_horizontal != prev_pos_horizontal:
-                    if new_pos_horizontal == Horizontal.UP:
-                        press("up")
-                    elif new_pos_horizontal == Horizontal.DOWN:
-                        press("down")
+            if new_pos_horizontal != prev_pos_horizontal:
+                if new_pos_horizontal == Horizontal.UP:
+                    press("up")
+                elif new_pos_horizontal == Horizontal.DOWN:
+                    press("down")
 
-                    pos_horizontal = new_pos_horizontal
-                    prev_pos_horizontal = new_pos_horizontal
+                pos_horizontal = new_pos_horizontal
+                prev_pos_horizontal = new_pos_horizontal
 
     # Rysowanie linii podziału
     cv2.line(frame, (left_part, upper_part), (left_part, lower_part), line_colour, 2)
@@ -105,11 +109,9 @@ while cap.isOpened():
     cv2.putText(frame, str(pos_vertical), (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, text_colour, 2)
     cv2.putText(frame, str(pos_horizontal), (50, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, text_colour, 2)
 
-    cv2.imshow("Pose Tracking", frame)
+    cv2.imshow("window", frame)
     if cv2.waitKey(25) & 0xFF == ord("q"):
         break
-    frame_counter += 1
-
 
 cap.release()
 cv2.destroyAllWindows()
